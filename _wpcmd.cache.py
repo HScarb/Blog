9967d5c58997067f7336bf58e5feca5cfdc52053
cnScarb <jjhfen00@163.com>
2016-03-11 15:33:11 +0800

    Updated a post about Git. Add a wpcmd cache.

---
+++ _wpcmd.cache.py
@@ -0,0 +1,205 @@
+{
+'post_tag': {
+'http': {
+'taxonomy': 'post_tag',
+'count': 1,
+'parent': '0',
+'name': 'http',
+'description': '',
+'slug': 'http',
+'group': '0',
+'taxonomy_id': '28',
+'id': '28'
+},
+'cocos2d-x': {
+'taxonomy': 'post_tag',
+'count': 0,
+'parent': '0',
+'name': 'cocos2d-x',
+'description': '',
+'slug': 'cocos2d-x',
+'group': '0',
+'taxonomy_id': '13',
+'id': '13'
+},
+'graph': {
+'taxonomy': 'post_tag',
+'count': 1,
+'parent': '0',
+'name': '图形学',
+'description': '计算机图形学',
+'slug': 'graph',
+'group': '0',
+'taxonomy_id': '21',
+'id': '21'
+},
+'blog': {
+'taxonomy': 'post_tag',
+'count': 1,
+'parent': '0',
+'name': '博客',
+'description': '关于博客',
+'slug': 'blog',
+'group': '0',
+'taxonomy_id': '23',
+'id': '23'
+},
+'md': {
+'taxonomy': 'post_tag',
+'count': 2,
+'parent': '0',
+'name': 'MarkDown',
+'description': 'MarkDown 文本语言',
+'slug': 'md',
+'group': '0',
+'taxonomy_id': '18',
+'id': '18'
+},
+'csharp': {
+'taxonomy': 'post_tag',
+'count': 0,
+'parent': '0',
+'name': 'C#',
+'description': '',
+'slug': 'csharp',
+'group': '0',
+'taxonomy_id': '12',
+'id': '12'
+},
+'algorithm': {
+'taxonomy': 'post_tag',
+'count': 0,
+'parent': '0',
+'name': '算法',
+'description': '',
+'slug': 'algorithm',
+'group': '0',
+'taxonomy_id': '16',
+'id': '16'
+},
+'git': {
+'taxonomy': 'post_tag',
+'count': 0,
+'parent': '0',
+'name': 'Git',
+'description': 'Git和Github相关',
+'slug': 'git',
+'group': '0',
+'taxonomy_id': '33',
+'id': '33'
+},
+'acm': {
+'taxonomy': 'post_tag',
+'count': 2,
+'parent': '0',
+'name': 'ACM',
+'description': 'hduacm',
+'slug': 'acm',
+'group': '0',
+'taxonomy_id': '17',
+'id': '17'
+},
+'crawler': {
+'taxonomy': 'post_tag',
+'count': 5,
+'parent': '0',
+'name': '爬虫',
+'description': '网络爬虫',
+'slug': 'crawler',
+'group': '0',
+'taxonomy_id': '27',
+'id': '27'
+},
+'cpp': {
+'taxonomy': 'post_tag',
+'count': 1,
+'parent': '0',
+'name': 'C++',
+'description': '',
+'slug': 'cpp',
+'group': '0',
+'taxonomy_id': '11',
+'id': '11'
+},
+'python': {
+'taxonomy': 'post_tag',
+'count': 5,
+'parent': '0',
+'name': 'python',
+'description': '',
+'slug': 'python',
+'group': '0',
+'taxonomy_id': '14',
+'id': '14'
+},
+'ds': {
+'taxonomy': 'post_tag',
+'count': 0,
+'parent': '0',
+'name': '数据结构',
+'description': '',
+'slug': 'ds',
+'group': '0',
+'taxonomy_id': '15',
+'id': '15'
+}
+},
+'category': {
+'note': {
+'taxonomy': 'category',
+'count': 5,
+'parent': '0',
+'name': '笔记',
+'description': '需要记一下的东西。可能是自己写的也可能是别处搜刮的。',
+'slug': 'note',
+'group': '0',
+'taxonomy_id': '31',
+'id': '31'
+},
+'gadget': {
+'taxonomy': 'category',
+'count': 2,
+'parent': '0',
+'name': '小玩意',
+'description': '超小型的“项目”',
+'slug': 'gadget',
+'group': '0',
+'taxonomy_id': '30',
+'id': '30'
+},
+'uncategorized': {
+'taxonomy': 'category',
+'count': 2,
+'parent': '0',
+'name': '未分类',
+'description': '',
+'slug': 'uncategorized',
+'group': '0',
+'taxonomy_id': '1',
+'id': '1'
+},
+'exp': {
+'taxonomy': 'category',
+'count': 5,
+'parent': '0',
+'name': '心得',
+'description': '自己捣鼓出来的心得，拿来分享。',
+'slug': 'exp',
+'group': '0',
+'taxonomy_id': '32',
+'id': '32'
+},
+'other': {
+'taxonomy': 'category',
+'count': 0,
+'parent': '0',
+'name': '其他',
+'description': '乱七八糟',
+'slug': 'other',
+'group': '0',
+'taxonomy_id': '10',
+'id': '10'
+}
+},
+'cachefile': 'I:\\WS-Blog\\_wpcmd.cache.py'
+}
